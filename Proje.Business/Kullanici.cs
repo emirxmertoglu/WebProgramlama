013a5ea9be47ddb2960e46be9401deb68408ef3a
emirxmertoglu <emirxmertoglu@outlook.com>
2021-04-15 21:23:58 +0300

    Added get users function

---
+++ Proje.Business/Kullanici.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje.Business
+{
+    public class Kullanici
+    {
+        public List<Proje.DataAccess.user> KullanicilariGetir()
+        {
+            Proje.DataAccess.WebProgramlamaEntities ent = new DataAccess.WebProgramlamaEntities();
+            var sonuc = ent.user.ToList();
+            return sonuc;
+        }
+    }
+}
